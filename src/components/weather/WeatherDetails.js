5e58d9878255a38d11fcb6df5888925ae90eb925
P1wP <jokke90@gmail.com>
2021-02-03 21:21:37 +0100

    Styling icons

--- src/components/weather/WeatherDetails.js
+++ src/components/weather/WeatherDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import sunCloud from "../../icons/SVG/sunCloud.svg";
 
 
 function WeatherDetails ({weather, day}){
@@ -10,11 +11,12 @@ function WeatherDetails ({weather, day}){
             </div>
 
             <div className="Weather__image">
-                {weather && <p className="Weather__image--icon">IMAGE: {weather.daily[0].weather[0].main}</p>}
+                <img className="Weather__image--icon" alt="icon of weather" src={sunCloud} />
+                {weather && <p className="Weather__image--text">{weather.daily[0].weather[0].main}</p>}
             </div>
             
             <div className="Weather__temp">
-                {weather && <p className="Weather__temp--text">TEMP: {weather.current.temp }</p>}
+                {weather && <p className="Weather__temp--text">{weather.current.temp } &deg;C </p>}
             </div>
             
         </div>
